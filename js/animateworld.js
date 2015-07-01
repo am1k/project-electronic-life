febcf36ccea715bd861013e551ce69e6bf2e1ba6
am1k <vitaliy_bog@mail.ru>
2015-07-01 15:01:06 +0300

    change header's

--- js/animateworld.js
+++ js/animateworld.js
@@ -1,3 +1,9 @@
+/**
+ * @license eLife 1.0 Copyright (c) 2015, Vitaliy Bogoroditskiy All Rights Reserved.
+ * Available via the MIT or new BSD license.
+ * see: https://github.com/am1k/project-electronic-life for details
+ */
+
 define(['areaAction/main'], function(world) {
   "use strict";
 
--- js/areaAction/vector.js
+++ js/areaAction/vector.js
@@ -1,5 +1,7 @@
 /**
- * Created by v.bogoroditskiy.
+ * @license eLife 1.0 Copyright (c) 2015, Vitaliy Bogoroditskiy All Rights Reserved.
+ * Available via the MIT or new BSD license.
+ * see: https://github.com/am1k/project-electronic-life for details
  */
 
 define(function () {
